7dd15248f1b5b20d0cc109604ddb30f2a8ee88f1
amenski <amatwd@gmail.com>
2020-03-25 19:24:22 +0100

    refactoring

--- src/main/java/et/commcare/covid19/task/dto/AppID.java
+++
@@ -1,9 +0,0 @@
-package et.commcare.covid19.task.dto;
-
-import java.util.*;
-import java.io.IOException;
-import com.fasterxml.jackson.annotation.*;
-
-public enum AppID {
-//Intentinally left this blank
-}
