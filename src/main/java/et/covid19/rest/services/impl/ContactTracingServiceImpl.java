0db8227775f94b57e260bdd11a6f7aaa38c797d7
aman <amantwd@gmail.com>
2020-05-01 19:04:22 +0200

    contact tracing tree with additional fields

--- src/main/java/et/covid19/rest/services/impl/ContactTracingServiceImpl.java
+++ src/main/java/et/covid19/rest/services/impl/ContactTracingServiceImpl.java
@@ -1,8 +1,13 @@
 package et.covid19.rest.services.impl;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +16,7 @@ import org.springframework.stereotype.Service;
 import et.covid19.rest.annotations.EthLoggable;
 import et.covid19.rest.dal.model.ContactTracing;
 import et.covid19.rest.dal.repositories.ContactTracingRepository;
+import et.covid19.rest.dal.util.ContactTracingProjection;
 import et.covid19.rest.services.IContactTracingService;
 import et.covid19.rest.util.exception.EthException;
 import et.covid19.rest.util.tree.GenericTree;
@@ -36,63 +42,60 @@ public class ContactTracingServiceImpl extends AbstractService implements IConta
 			 * ContactTracing table holds the format { parentCode:[childCode1#childCode2....] } 
 			 * 
 			 * Flow: 
-			 * 1. findAll the parent:child and get the row with `code` == `parentCode`
-			 * 2. Get the parent
-			 * 3. Create(populate) TreeNode from its children and return it.
-			 * 4. Create(populate) TreeNode from the parent
-			 * 5. build the overall tree
+			 * 1. findAll the parent:child
+			 * 2. Get and process the parent with immediate children only
+			 * 3. Add TreeNode to list with parent:child structure
+			 * 4. build the overall tree
 			 *  
 			 */
-			ContactTracing pcase = null;
+			TreeNode<ContactTracingProjection> parentNode = null;
 			//1
 			List<ContactTracing> pcases = contactTracingRepository.findAll();
-			List<TreeNode<ContactTracing>> listOfCt = new ArrayList<>(100); 
+			List<TreeNode<ContactTracingProjection>> listOfCt = new ArrayList<>(100); 
 			for(ContactTracing single : pcases) {
 			    //2
 				if(code.equals(single.getParentCaseCode())){
-					pcase = single;
+					parentNode = populateChildren(single);
+					continue;
 				}
 				//3
 				listOfCt.add(populateChildren(single));
 			}
 			//4
-			TreeNode<ContactTracing> parentNode = populateChildren(pcase);
-			//5
 			return buildTree(parentNode, listOfCt);
 		}catch (Exception ex) {
 			throw ex;
 		}
 	}
 	
-	private TreeNode<ContactTracing> populateChildren(ContactTracing ct) {
+	// for each parent with children, make a join to pui_info
+	private TreeNode<ContactTracingProjection> populateChildren(ContactTracing ct) {
 		if(Objects.isNull(ct))
 			return null;
 		
-		TreeNode<ContactTracing> node = new TreeNode<>(ct);
+		Set<String> ids = new HashSet<>();
+		ids.add(ct.getParentCaseCode());
+		ids.addAll(ct.getChildren().stream().map(ContactTracing::getParentCaseCode).collect(Collectors.toSet()));
+		
+		List<ContactTracingProjection> parentChildProjections = contactTracingRepository.getContactTracingProjectionByCode(ids);
+		Map<String, ContactTracingProjection> projectionMap = parentChildProjections.stream().collect(Collectors.toMap(ContactTracingProjection::getCode, Function.identity()));
+		
+		// get parent and child ctProjection once, and loop to populate
+		TreeNode<ContactTracingProjection> node = new TreeNode<>(projectionMap.get(ct.getParentCaseCode()));
 		ct.getChildren().stream().forEach(child -> {
-			node.getChildren().add(new TreeNode<ContactTracing>(child));
+			node.getChildren().add(new TreeNode<ContactTracingProjection>(projectionMap.get(child.getParentCaseCode())));
 		});
 		return node;
 	}
-	//TODO how to add additional fields from PUI_INFO?
+	
 	@EthLoggable
-	private String buildTree(TreeNode<ContactTracing> parent, List<TreeNode<ContactTracing>> list) {
+	private String buildTree(TreeNode<ContactTracingProjection> parent, List<TreeNode<ContactTracingProjection>> list) {
 		String json = null;
 		try {
-			GenericTree<ContactTracing> genericTree = new GenericTree<ContactTracing>().buildTree(parent, list);
-			//remove extra children block from parent.data
-			genericTree.getRoot().getData().getChildren().clear(); 
+			GenericTree<ContactTracingProjection> genericTree = new GenericTree<ContactTracingProjection>().buildTree(parent, list);
 			json = genericTree.getJSON();
 		} catch (Exception ex) {
 		}
 		return json;
-	}
-
-	// build tree for PuiInfo from ContactTracing tree
-	// used this approach not to query the PuiInfo table for every parentCases and their corresponding children
-	// the ContactTracing table holds only parent:child structure which will then be used here to build
-	// the puiInfo tree which is the final output.
-	private void constructPuiInfoFromTree(GenericTree<ContactTracing> tree) {
-	    
 	}
 }
