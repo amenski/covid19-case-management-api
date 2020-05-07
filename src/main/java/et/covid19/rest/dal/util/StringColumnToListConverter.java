8e027d91d119ec90c4dd16c8193f88158659a7e0
aman <amantwd@gmail.com>
2020-05-07 15:17:46 +0200

    fix attribute attribute convertor

--- src/main/java/et/covid19/rest/dal/util/StringColumnToListConverter.java
+++ src/main/java/et/covid19/rest/dal/util/StringColumnToListConverter.java
@@ -1,10 +1,13 @@
 package et.covid19.rest.dal.util;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import javax.persistence.AttributeConverter;
 import javax.persistence.Convert;
+
+import org.apache.commons.lang3.StringUtils;
 
 import et.covid19.rest.util.EthConstants;
 
@@ -13,6 +16,9 @@ public class StringColumnToListConverter implements AttributeConverter<List<Stri
 
     @Override
     public String convertToDatabaseColumn(List<String> attribute) {
+        if(attribute == null || attribute.isEmpty())
+            return StringUtils.EMPTY;
+        
         StringBuilder sb = new StringBuilder();
         attribute.stream().forEach(val -> sb.append(val).append(EthConstants.ATTRIBUTE_SEPARATOR));
         sb.setLength(sb.length() - 1);
@@ -21,6 +27,9 @@ public class StringColumnToListConverter implements AttributeConverter<List<Stri
 
     @Override
     public List<String> convertToEntityAttribute(String dbData) {
+        if(StringUtils.isBlank(dbData))
+            return new ArrayList<>(1);
+        
         return Arrays.asList(dbData.split(EthConstants.ATTRIBUTE_SEPARATOR));
     }
 
